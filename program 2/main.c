3df4286c81dd9acb509dfe47860b23dd78c6edee
ajinkyapardhe11 <110289553+ajinkyapardhe11@users.noreply.github.com>
2022-07-30 18:10:58 +0530

    Add files via upload

---
+++ program 2/main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>
+void main()
+{
+    int x=156;
+    float f=94.36;
+    char ch = 'C';
+    
+    printf("%d\n",x);
+    printf("%f\n",f);
+    printf("%c\n",ch);
+}
