4c55e3201f9317aee99c84747041f22ec8fe8637
ajinkyapardhe11 <110289553+ajinkyapardhe11@users.noreply.github.com>
2022-07-30 18:12:06 +0530

    Add files via upload

---
+++ program 1/main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+void main()
+{
+int age=20;
+float petrolprice=105.64;
+double dollar=79.20;
+char wing='A';
+
+printf ("%d\n",age);
+printf ("%f\n",petrolprice);
+printf ("%f\n",dollar);
+printf ("%c\n",wing);
+}
